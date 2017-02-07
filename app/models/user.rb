0dfbc91d88601aec840800f30ee3e705b1a68709
youkaiantena <apple.kitaro@gmail.com>
2017-02-07 22:11:11 +0900

    :+1: Userモデルに、セキュアなパスワードをもたせた

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,8 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
 
   has_secure_password
+
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
 end
