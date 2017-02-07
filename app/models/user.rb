116ee1cc4ff59e220251ab77dd3cbbe2224761a4
youkaiantena <apple.kitaro@gmail.com>
2017-02-07 21:31:03 +0900

    :+1: email属性を小文字に変換してメールアドレスの一意性を保証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  #before_save { self.email = email.downcase }
+  before_save { email.downcase! }
+
   #validates(:name, { presence: true })
   validates :name,
             presence: true,
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,4 +56,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not dupulicate_user.valid?
   end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
